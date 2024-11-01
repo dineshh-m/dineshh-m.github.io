e8471adaadb73693d44252916ae7ac62bf2e38a8
Dinesh M <dineshmoorthy101@gmail.com>
2024-11-01 19:43:51 +0530

    add workflows

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import PostListRSC from './components/post-list/rsc';
-import Link from 'next/link';
+import Image from "next/image";
+import styles from "./page.module.css";
+import PostListRSC from "./components/post-list/rsc";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <p>
-        Hi, I am a Dinesh! I am software developer tryin&apos; to learn lot of
+        Hi, I am Dinesh! I am a Software Developer tryin&apos; to learn lot of
         things. I code in JavaScript, Golang, Java, Python and sometimes in C as
-        well. I am interested in Web apps, Linux, Compilers, System Programming, Databases
-        and... you get the idea right?
+        well. I am interested in Web apps, Linux, Compilers, System Programming,
+        Databases and... you get the idea right?
       </p>
       <section>
         <h2>Blogs</h2>
